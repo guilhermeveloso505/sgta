1aff593e88f931313d04e7f129deb974ac8e0cd2
guilherme veloso <velosoguilherme73@gmail.com>
2026-04-06 21:37:48 -0300

    Atualização

--- backend/tarefas/urls.py
+++ backend/tarefas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import listar_tarefas
+from .views import listar_tarefas_por_status
 
 urlpatterns = [
     path('tarefas/', listar_tarefas),
+    path('tarefas/status/<str:status>', listar_tarefas_por_status)
 ]
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -5,3 +5,12 @@ def listar_tarefas(request):
     tarefas = Tarefa.objects.all().values()
     return JsonResponse(list[any](tarefas), safe=False)
 # Create your views here.
+
+def listar_tarefas_por_status(request, status):
+    status_validos = [choice[0] for choice in Tarefa.STATUS_CHOICES]
+    
+    if status not in status_validos:
+        return JsonResponse({'erro': 'status inválido.'}, status=400)
+    
+    tarefas = Tarefa.objects.filter(status=status).values() # ---> filter = Realiza o filtro para a consulta, no caso anterior status = status.
+    return JsonResponse(list[any](tarefas), safe=False)
